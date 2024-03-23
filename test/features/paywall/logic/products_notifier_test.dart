21d1cdd72fbbe6b816ad208b4bcce8d4d8cf7abe
RuslanTsitser <cicer_ruslan@mail.ru>
2024-03-23 21:53:31 +0300

    fake async and error case

--- test/features/paywall/logic/products_notifier_test.dart
+++ test/features/paywall/logic/products_notifier_test.dart
@@ -1,10 +1,10 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
-
+import 'package:fake_async/fake_async.dart';
 import 'package:flutter_application_1/features/paywall/domain/paywall_product.dart';
 import 'package:flutter_application_1/features/paywall/domain/paywall_repository.dart';
 import 'package:flutter_application_1/features/paywall/logic/products_notifier.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
 @GenerateNiceMocks([MockSpec<PaywallRepository>()])
 import 'products_notifier_test.mocks.dart';
@@ -13,10 +13,17 @@ void main() {
   group('ProductsNotifier', () {
     late ProductsNotifier productsNotifier;
     late MockPaywallRepository mockPaywallRepository;
-
+    const mockProducts = [
+      PaywallProduct(title: 'Product 1', price: '10'),
+      PaywallProduct(title: 'Product 2', price: '20'),
+    ];
     setUp(() {
       mockPaywallRepository = MockPaywallRepository();
       productsNotifier = ProductsNotifier(mockPaywallRepository);
+      when(mockPaywallRepository.getProducts()).thenAnswer((_) async {
+        await Future.delayed(const Duration(seconds: 1));
+        return mockProducts;
+      });
     });
 
     tearDown(() {
@@ -25,26 +32,31 @@ void main() {
     });
 
     test('fetchProducts should update products list and loading status', () async {
-      const mockProducts = [
-        PaywallProduct(title: 'Product 1', price: '10'),
-        PaywallProduct(title: 'Product 2', price: '20'),
-      ];
+      fakeAsync((async) {
+        expect(productsNotifier.loading, false);
+        expect(productsNotifier.products, []);
 
-      when(mockPaywallRepository.getProducts()).thenAnswer((_) async {
-        await Future.delayed(const Duration(seconds: 1));
-        return mockProducts;
+        productsNotifier.fetchProducts();
+        expect(productsNotifier.loading, true);
+        expect(productsNotifier.products, []);
+
+        async.elapse(const Duration(seconds: 3));
+        expect(productsNotifier.loading, false);
+        expect(productsNotifier.products, mockProducts);
       });
-
-      expect(productsNotifier.loading, false);
-      expect(productsNotifier.products, []);
-
-      productsNotifier.fetchProducts();
-      expect(productsNotifier.loading, true);
-      expect(productsNotifier.products, []);
-
-      await Future.delayed(const Duration(seconds: 3));
-      expect(productsNotifier.loading, false);
-      expect(productsNotifier.products, mockProducts);
     });
+
+    test(
+      'fetchProducts should return exception',
+      () async {
+        when(mockPaywallRepository.getProducts()).thenThrow(Exception('no internet'));
+
+        expect(productsNotifier.loading, false);
+        expect(productsNotifier.products, []);
+
+        productsNotifier.fetchProducts();
+        expect(productsNotifier.isError, true);
+      },
+    );
   });
 }
